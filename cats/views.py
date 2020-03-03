e02a4b63e5bf8c2fe4702a6d7fa1ad574c1b8f9e
Brent Muha <800@holbertonschool.com>
2020-03-03 14:21:42 -0800

    Create base html and css

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 
+cats = [
+    {
+        'id': 1,
+        'photo': 'https://i.imgur.com/RSXl1iN.jpg',
+        'name': 'Gary',
+        'age': 9,
+        'breed': 'American Shorthair',
+        'color': 'orange'
+    },
+    {
+        'id': 2,
+        'photo': 'https://i.imgur.com/M5fQpjG.jpg',
+        'name': 'Thor',
+        'age': 4,
+        'breed': 'Unknown',
+        'color': 'black, white'
+    }
+]
+
 
 def home(request):
-    return render(request, 'cats/home.html')
+    context = {
+        'cats': cats,
+    }
+    return render(request, 'cats/home.html', context)
 
 
 def about(request):
-    return render(request, 'cats/about.html')
+    return render(request, 'cats/about.html', {'title': 'About'})
