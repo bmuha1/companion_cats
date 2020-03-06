3dc73ca74f493fa753c3ff06025215ed65e02992
Brent Muha <800@holbertonschool.com>
2020-03-06 15:04:24 -0800

    Switch from dummy data to database

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=20)
+    photo = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
