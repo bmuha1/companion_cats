8626c890e00c2e85115afeed1bdf64d6fe0829e6
Brent Muha <800@holbertonschool.com>
2020-03-11 15:00:29 -0700

    Remove page for adding cats

--- cats/urls.py
+++ cats/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='cats-home'),
     path('about/', views.about, name='cats-about'),
-    path('random/', views.random, name='cats-random'),
-    path('add/', views.add, name='cats-add')
+    # path('random/', views.random, name='cats-random'),
+    # path('add/', views.add, name='cats-add')
 ]
