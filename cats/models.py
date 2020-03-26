e8a41d3ba3f1552091721ad65d5059af9ad3f780
Brent Muha <45921678+bmuha1@users.noreply.github.com>
2020-03-26 10:41:14 -0700

    Add documentation for Cat class

--- cats/models.py
+++ cats/models.py
@@ -3,6 +3,16 @@ from django import forms
 
 
 class Cat(models.Model):
+    """
+    Cat object, contains the following attributes
+    name: string
+    photo: string containing a URL
+    age: int
+    breed: string
+    latitude: float
+    longitude: float
+    available: bool, default is True
+    """
     name = models.CharField(max_length=20)
     photo = models.CharField(max_length=100)
     age = models.IntegerField()
@@ -11,5 +21,7 @@ class Cat(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     available = models.BooleanField(default=True)
 
+    
     def __str__(self):
+        # Return the name of the Cat
         return self.name
